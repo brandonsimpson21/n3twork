bf9b0b525426ab3f8cc6f372ececfeabe38abdda
Brandon Simpson <brandonsimpson21@gmail.com>
2024-01-26 13:12:10 -0800

    feat(connection manager) crypto errors

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,21 @@ use thiserror::Error;
 pub enum N3tworkError {
     #[error("data store disconnected")]
     IOError(#[from] std::io::Error),
+    #[error("send error {0}")]
+    SendError(String),
+    #[error("recv error {0}")]
+    RecvError(String),
     #[error("Internal Error {0}")]
+    InternalError(String),
+    #[error("Unknown Error {0}")]
+    UnknownError(String),
+}
+
+#[derive(Error, Debug)]
+pub enum CryptoError {
+    #[error("signature error {0}")]
+    SignatureError(String),
+    #[error("internal error {0}")]
     InternalError(String),
     #[error("Unknown Error {0}")]
     UnknownError(String),
@@ -18,6 +32,12 @@ impl From<Box<dyn std::error::Error>> for N3tworkError {
 
 impl From<anyhow::Error> for N3tworkError {
     fn from(error: anyhow::Error) -> Self {
+        N3tworkError::InternalError(error.to_string())
+    }
+}
+
+impl From<CryptoError> for N3tworkError {
+    fn from(error: CryptoError) -> Self {
         N3tworkError::InternalError(error.to_string())
     }
 }
